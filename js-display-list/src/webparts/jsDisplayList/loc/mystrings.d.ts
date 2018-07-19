9d8632db58d8bbcd64c6f3140a82d1debeb87997
Pratik Shahi <pratik@AV.LOCAL>
2018-07-19 13:23:05 +0545

    jquery-List-Data

---
+++ js-display-list/src/webparts/jsDisplayList/loc/mystrings.d.ts
@@ -0,0 +1,12 @@
+declare interface IJsDisplayListWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  DataGroup:string;
+  Loading:string
+}
+
+declare module 'JsDisplayListWebPartStrings' {
+  const strings: IJsDisplayListWebPartStrings;
+  export = strings;
+}
